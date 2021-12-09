425fe645539bde5f32af04561fccafce08b36dbb
Sunny@dxl2 <kwoksun2@dxl2.ncsa.illinois.edu>
2021-12-09 16:17:23 -0600

    fix relative import

--- HaloEGNN_model.py
+++ HaloEGNN_model.py
@@ -1,19 +1,19 @@
-from EGNN_layers import *
+from .EGNN_layers import *
 
 class EGNN_Model(torch.nn.Module):
   def __init__(self,
-               in_node_nf=8, 
-               in_edge_nf=3, 
-               hidden_nf=32, 
+               in_node_nf=8,
+               in_edge_nf=3,
+               hidden_nf=32,
                n_layers=4,
                target_shape= 1):
     super().__init__()
-    self.egnn = EGNN(in_node_nf=in_node_nf, 
-                     in_edge_nf=in_edge_nf, 
-                     hidden_nf=hidden_nf, 
+    self.egnn = EGNN(in_node_nf=in_node_nf,
+                     in_edge_nf=in_edge_nf,
+                     hidden_nf=hidden_nf,
                      n_layers=n_layers,
                      device='cpu')
-    
+
     self.hidden_nf = hidden_nf
     act_fn = nn.LeakyReLU(0.2)
     self.node_dec = nn.Sequential(nn.Linear(self.hidden_nf, self.hidden_nf),
@@ -23,7 +23,7 @@ class EGNN_Model(torch.nn.Module):
     self.graph_dec = nn.Sequential(nn.Linear(self.hidden_nf, self.hidden_nf),
                                     act_fn,
                                     nn.Linear(self.hidden_nf, target_shape))
-    
+
   def forward(self, nodes, pos_feat, edges, edge_attr):
 
     h,x = self.egnn(nodes, pos_feat, edges, edge_attr)
